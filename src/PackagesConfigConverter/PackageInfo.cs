f0f9ba449be1d4e597af18531b932fa2c06cb08a
David Federman <dfederm@microsoft.com>
2024-08-08 14:18:58 -0700

    Reuse package information for unique packages (#50)

--- src/PackagesConfigConverter/PackageInfo.cs
+++ src/PackagesConfigConverter/PackageInfo.cs
@@ -2,11 +2,8 @@
 //
 // Licensed under the MIT license.
 
-using Microsoft.Build.Construction;
-using NuGet.Frameworks;
 using NuGet.Packaging;
 using NuGet.Packaging.Core;
-using NuGet.Versioning;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -14,41 +11,28 @@ using System.Linq;
 
 namespace PackagesConfigConverter
 {
-    internal class PackageReference : NuGet.Packaging.PackageReference
+    internal sealed class PackageInfo
     {
         private IReadOnlyCollection<string> _installedPackageFolderNames;
 
-        public PackageReference(PackageIdentity identity, NuGetFramework targetFramework, bool userInstalled, bool developmentDependency, bool requireReinstallation, VersionRange allowedVersions, PackagePathResolver packagePathResolver, VersionFolderPathResolver versionFolderPathResolver)
-            : base(identity, targetFramework, userInstalled, developmentDependency, requireReinstallation, allowedVersions)
+        public PackageInfo(PackageIdentity identity, PackagePathResolver packagePathResolver, VersionFolderPathResolver versionFolderPathResolver)
         {
-            InstalledPackageFilePath = PackagePathHelper.GetInstalledPackageFilePath(PackageIdentity, packagePathResolver ?? throw new ArgumentNullException(nameof(packagePathResolver)));
+            Identity = identity;
+
+            InstalledPackageFilePath = PackagePathHelper.GetInstalledPackageFilePath(Identity, packagePathResolver ?? throw new ArgumentNullException(nameof(packagePathResolver)));
 
             RepositoryInstalledPath = Path.GetDirectoryName(InstalledPackageFilePath);
 
             GlobalInstalledPath = Path.GetFullPath(versionFolderPathResolver.GetInstallPath(identity.Id, identity.Version));
         }
 
-        public IEnumerable<ProjectElement> AllElements => AnalyzerItems.Cast<ProjectElement>().Concat(AssemblyReferences).Concat(Imports);
-
-        public List<ProjectItemElement> AnalyzerItems { get; } = new ();
-
-        public List<ProjectItemElement> AssemblyReferences { get; } = new ();
-
-        public bool GeneratePathProperty { get; set; }
-
         public string GlobalInstalledPath { get; }
-
-        public List<ProjectImportElement> Imports { get; } = new ();
 
         public string InstalledPackageFilePath { get; }
 
         public IReadOnlyCollection<string> InstalledPackageFolderNames => _installedPackageFolderNames ??= GetPackageFolderNames();
 
-        public bool IsMissingTransitiveDependency { get; set; }
-
-        public string PackageId => PackageIdentity.Id;
-
-        public NuGetVersion PackageVersion => PackageIdentity.Version;
+        public PackageIdentity Identity { get; }
 
         public string RepositoryInstalledPath { get; }
 
